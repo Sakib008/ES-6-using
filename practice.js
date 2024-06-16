01f6a0ec20adf20aa644f81c56ae742efe29a0f8
Sakib008 <mdshakib1902@gmail.com>
2024-06-16 10:26:48 +0530

    Update practice.js
    commit practice

--- practice.js
+++ practice.js
@@ -45,4 +45,40 @@ const printSomething = a =>{
     console.log('Sqare a //...........');
     return a*a;
 }
-// console.log(printSomething(5));
+console.log(printSomething(5));
+
+
+// 5. Const with objects
+
+const obj = {a: 1, b: 2}
+obj.a = 22;
+
+
+// will get error ( declaration again)
+const obj2 = {a:3, b:4}
+// obj1 = obj2 ;
+// console.log(obj1)
+
+function getNum(){
+    return 2;
+}
+// console.log(getNum())
+
+
+// console.log(isBigger(3,2))
+
+
+
+// rest Example 
+const rest = (a, ...rest)=>{
+    console.log(a,rest);
+}
+// console.log(1,3,2,3,4,5,6,7,89,0,2);
+
+
+// Rest Example with Array
+const array = [1,2,4,5,7,9,9,0,1,2,4];
+const spreadOperator = ([first,second,...rest]) =>{
+    console.log(first,second,rest)
+}
+console.log(spreadOperator(...array,11,2,33,334,53,34,53,22,13));
